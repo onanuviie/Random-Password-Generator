ac574b6b925140862da42c89fdfb3c3422928a07
onanuviie <nuviesilvet@gmail.com>
2023-03-04 12:21:15 +0100

    shuffled the password

--- app.js
+++ app.js
@@ -36,7 +36,28 @@ function createPassword() {
     password.push(specialCharacter1)
     let specialCharacter2 = String.fromCharCode(generateRandom(91, 96))
     password.push(specialCharacter2)
+
+    shuffle(password)
 }
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  
 
 function showPassword() {
     createPassword()
